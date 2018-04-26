bb090c12d095a5df624b4cfc1fffd8055c72695a
Andrew Ingraham <aingraham@flatiron.com>
2018-04-26 01:42:31 -0400

    Adding more music

--- src/music/index.js
+++ src/music/index.js
@@ -1,3 +1,7 @@
 export default {
-  'Creepy': require('./lostMine.mp3')
+  'Creepy': require('./lostMine.mp3'),
+  'Halo': require('./Halo-Theme.mp3'),
+  'Boss Battle': require('./Final-Fantasy-7-Boss-Battle.mp3'),
+  'Stranger Things': require('./Stranger-Things-Theme.mp3'),
+  'Adventure': require('./Zelda-Intro.mp3'),
 }
